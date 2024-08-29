5a7079d34ce2cf16371854d79efde97272d54b77
JukisYuri <23130092@st.hcmuaf.edu.vn>
2024-08-30 00:05:03 +0700

    Add event listener for button

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("carrdBtn").addEventListener("click", function() {
+    window.open("https://jukisyuri.carrd.co", "_blank");
+});
+
+document.getElementById("discordBtn").addEventListener("click", function() {
+    window.open("https://discord.gg/R7sCWBTfcg", "_blank");
+});
